720b9612b43795df4fd0f455a2e727986ff50098
imam.basri <imam.basri@edi-indonesia.co.id>
2023-09-17 16:59:14 +0700

    first setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+      <div className='flex justify-center items-center w-[100vw] h-[100vh]'>
+          <h1 className='text-center font-sans text-2xl font-bold'>Note App | Dicoding</h1>
+      </div>
+  );
+}
+export default App
